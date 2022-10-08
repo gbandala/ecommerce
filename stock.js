24a3b060c13ed81332f35afffa94b79581aab0fa
Gabriel Bandala <bandala@outlook.com>
2022-10-08 11:14:47 -0500

    Proyecto Base
    Proyecto Base

---
+++ stock.js
@@ -0,0 +1,15 @@
+let stockProductos = [
+    {id: 1, nombre: "Short 1", tipo: "Short", cantidad: 1, desc: "Short Corto colores disponibles", precio: 1000, talla: "L", img: './img/buzo.jpg'},
+    {id: 2, nombre: "Playera 1", tipo: "Playera", cantidad: 1, desc: "Playera colores disponibles", precio: 1000, talla: "L", img: './img/remera.jpg'},
+    {id: 3, nombre: "Camisa 1", tipo: "camisa", cantidad: 1, desc: "Camisa colores disponibles", precio: 1000, talla: "L", img: './img/camisa.jpg'},
+    {id: 4, nombre: "Pantalon 1", tipo: "pantalon", cantidad: 1, desc: "Pantalon colores disponibles", precio: 1000, talla: "L", img: './img/pantalon.jpg'},
+    {id: 5, nombre: "Short 2", tipo: "Short", cantidad: 1, desc: "Short Corto colores disponibles", precio: 1000, talla: "L", img: './img/buzo.jpg'},
+    {id: 6, nombre: "Playera 2", tipo: "Playera", cantidad: 1, desc: "Playera colores disponibles", precio: 1000, talla: "L", img: './img/remera.jpg'},
+    {id: 7, nombre: "Camisa 2", tipo: "camisa", cantidad: 1, desc: "Camisa colores disponibles", precio: 1000, talla: "L", img: './img/camisa.jpg'},
+    {id: 8, nombre: "Pantalon 2", tipo: "pantalon", cantidad: 1, desc: "Pantalon colores disponibles", precio: 1000, talla: "L", img: './img/pantalon.jpg'},
+    {id: 9, nombre: "Short 3", tipo: "Short", cantidad: 1, desc: "Short Corto colores disponibles", precio: 1000, talla: "L", img: './img/buzo.jpg'},
+    {id: 10, nombre: "Playera 3", tipo: "Playera", cantidad: 1, desc: "Playera colores disponibles", precio: 1000, talla: "L", img: './img/remera.jpg'},
+    {id: 11, nombre: "Camisa 3", tipo: "camisa", cantidad: 1, desc: "Camisa colores disponibles", precio: 1000, talla: "L", img: './img/camisa.jpg'},
+    {id: 12, nombre: "Pantalon 3", tipo: "pantalon", cantidad: 1, desc: "Pantalon colores disponibles", precio: 1000, talla: "L", img: './img/pantalon.jpg'},
+  
+];
